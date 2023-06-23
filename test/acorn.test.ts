2f66398cbeb024622a826f1c4cee78cbda19f0d4
Nam Nguyen <nam.nguyen.node@gmail.com>
2023-06-23 10:29:56 -0300

    Add acorn and acorn-loose to the loop 😄

---
+++ test/acorn.test.ts
@@ -0,0 +1,40 @@
+import { parse } from "acorn";
+import { expect, test } from "vitest";
+
+test("acorn loose", () => {
+  expect(parse("1 + 1", { ecmaVersion: 2020 })).toMatchInlineSnapshot(`
+    Node {
+      "body": [
+        Node {
+          "end": 5,
+          "expression": Node {
+            "end": 5,
+            "left": Node {
+              "end": 1,
+              "raw": "1",
+              "start": 0,
+              "type": "Literal",
+              "value": 1,
+            },
+            "operator": "+",
+            "right": Node {
+              "end": 5,
+              "raw": "1",
+              "start": 4,
+              "type": "Literal",
+              "value": 1,
+            },
+            "start": 0,
+            "type": "BinaryExpression",
+          },
+          "start": 0,
+          "type": "ExpressionStatement",
+        },
+      ],
+      "end": 5,
+      "sourceType": "script",
+      "start": 0,
+      "type": "Program",
+    }
+  `);
+});
---
+++ test/acornLoose.test.ts
@@ -0,0 +1,71 @@
+import { parse } from "acorn-loose";
+import { expect, test } from "vitest";
+
+test("acorn loose", () => {
+  expect(parse("1 / * 4 )[2]", { ecmaVersion: 2020 })).toMatchInlineSnapshot(`
+    Node {
+      "body": [
+        Node {
+          "end": 7,
+          "expression": Node {
+            "end": 7,
+            "left": Node {
+              "end": 4,
+              "left": Node {
+                "end": 1,
+                "raw": "1",
+                "start": 0,
+                "type": "Literal",
+                "value": 1,
+              },
+              "operator": "/",
+              "right": Node {
+                "end": 4,
+                "name": "✖",
+                "start": 4,
+                "type": "Identifier",
+              },
+              "start": 0,
+              "type": "BinaryExpression",
+            },
+            "operator": "*",
+            "right": Node {
+              "end": 7,
+              "raw": "4",
+              "start": 6,
+              "type": "Literal",
+              "value": 4,
+            },
+            "start": 0,
+            "type": "BinaryExpression",
+          },
+          "start": 0,
+          "type": "ExpressionStatement",
+        },
+        Node {
+          "end": 12,
+          "expression": Node {
+            "elements": [
+              Node {
+                "end": 11,
+                "raw": "2",
+                "start": 10,
+                "type": "Literal",
+                "value": 2,
+              },
+            ],
+            "end": 12,
+            "start": 9,
+            "type": "ArrayExpression",
+          },
+          "start": 9,
+          "type": "ExpressionStatement",
+        },
+      ],
+      "end": 12,
+      "sourceType": "script",
+      "start": 0,
+      "type": "Program",
+    }
+  `);
+});
