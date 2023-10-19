9e62740b8c371f639bc87bd774aa2468973fad4c
Nam Nguyen <nam.nguyen.node@gmail.com>
2023-10-19 15:06:49 -0300

    uki 😉

--- test/polaris.test.ts
+++ test/polaris.test.ts
@@ -1,15 +1,7 @@
 // import { DividerProps} from "@shopify/polaris";
+import { writeFileSync } from "fs";
 import ts from "typescript";
 import { describe, it } from "vitest";
-
-// members is now ["a", "b", "c"]
-
-// const polarisFile = fs.readFileSync(
-//   "node_modules/@shopify/polaris/build/ts/src/components/Divider/Divider.d.ts",
-//   "utf8"
-// );
-
-// console.log(polarisFile);
 
 describe("Polaris test", () => {
   it("should work", () => {
@@ -18,9 +10,10 @@ describe("Polaris test", () => {
     // Get AST and find union node
     const program = ts.createProgram(
       [
-        "node_modules/@shopify/polaris/build/ts/src/components/Divider/Divider.d.ts",
+        "node_modules/@shopify/polaris/build/ts/src/components/Box/Box.d.ts",
+        // "node_modules/@shopify/polaris/build/ts/src/index.d.ts",
       ],
-      {},
+      {}
     );
 
     for (const sourceFile of program.getSourceFiles()) {
@@ -28,21 +21,13 @@ describe("Polaris test", () => {
         sourceFile.isDeclarationFile &&
         sourceFile.fileName.includes("@shopify")
       ) {
-        // ts.SyntaxKind.NumberKeyword
-        ts.forEachChild(sourceFile, (child) => {
-          if (ts.isInterfaceDeclaration(child)) {
-            if (child.name.escapedText === "DividerProps") {
-              // console.log(child.members[0].type.members[0]);
-            }
-          }
-
-          // if (ts.isTypeAliasDeclaration(child)) {
-          //   if (child.name.escapedText === "ColorBorderAlias") {
-          //     console.log(child.type.types.map((type) => type.literal.text));
-          //   }
-          // }
-        });
+        console.log(sourceFile.fileName);
       }
     }
+
+    // Write data to result.json
+    const data = { hello: "world" };
+    const json = JSON.stringify(data, null, 2);
+    writeFileSync("result.json", json);
   });
 });
