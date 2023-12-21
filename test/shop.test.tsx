1932516e4587fc29177c7b1fb246e2fda7656e0f
Nam Nguyen <nam.nguyen.node@gmail.com>
2023-12-21 18:41:55 -0400

    Format all things 🚀

--- test/shop.test.tsx
+++ test/shop.test.tsx
@@ -1,7 +1,7 @@
-import React from 'react';
-import {mount} from '@shopify/react-testing';
+import { mount } from "@shopify/react-testing";
+import React from "react";
 
-const ClickCounter = ({defaultCount}) => {
+const ClickCounter = ({ defaultCount }) => {
   const [count, setCount] = React.useState(0);
 
   return (
@@ -10,28 +10,28 @@ const ClickCounter = ({defaultCount}) => {
       <button onClick={() => setCount(count + 1)}>click</button>
     </div>
   );
-}
+};
 
-describe.only('<ClickCounter />', () => {
-  it('allows us to set props', () => {
+describe("<ClickCounter />", () => {
+  it("allows us to set props", () => {
     const wrapper = mount(<ClickCounter defaultCount={0} />);
-    
-    expect(wrapper.props.defaultCount).toBe(0);
-    expect(wrapper.text()).toBe('count: 0 click');
-    wrapper.setProps({defaultCount: 1});
-    expect(wrapper.props.defaultCount).toBe(1);
-    expect(wrapper.text()).toBe('count: 1 click');
 
-    wrapper.find('button').trigger('onClick');
-    wrapper.find('button').trigger('onClick');
-    expect(wrapper.text()).toBe('count: 2 click');
+    expect(wrapper.props.defaultCount).toBe(0);
+    expect(wrapper.text()).toBe("count: 0 click");
+    wrapper.setProps({ defaultCount: 1 });
+    expect(wrapper.props.defaultCount).toBe(1);
+    expect(wrapper.text()).toBe("count: 1 click");
+
+    wrapper.find("button").trigger("onClick");
+    wrapper.find("button").trigger("onClick");
+    expect(wrapper.text()).toBe("count: 2 click");
   });
 
-  it('triggers handlers', () => {
+  it("triggers handlers", () => {
     const wrapper = mount(<ClickCounter defaultCount={0} />);
-    wrapper.find('button').trigger('onClick');
-    wrapper.find('button').trigger('onClick');
-    wrapper.find('button').trigger('onClick');
-    expect(wrapper.text()).toBe('count: 3 click');
+    wrapper.find("button").trigger("onClick");
+    wrapper.find("button").trigger("onClick");
+    wrapper.find("button").trigger("onClick");
+    expect(wrapper.text()).toBe("count: 3 click");
   });
 });
