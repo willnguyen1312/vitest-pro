a9b45a621ca961af991d3a4b9ccffd726c363271
Nam Nguyen <nam.nguyen.node@gmail.com>
2024-01-15 19:25:22 -0400

    Add faker.js 💞

---
+++ test/faker.test.ts
@@ -0,0 +1,10 @@
+import { faker } from "@faker-js/faker";
+import { describe, expect, it } from "vitest";
+
+describe("faker", () => {
+  it("should be ok", () => {
+    const randomNum = faker.number.int({ min: 0, max: 2 });
+
+    expect(randomNum).toBeGreaterThanOrEqual(0);
+  });
+});
