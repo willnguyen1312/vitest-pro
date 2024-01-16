da5ba6a736900e98e16b52021d23c8e66f9b44f6
Nam Nguyen <nam.nguyen.node@gmail.com>
2024-01-16 17:40:20 -0400

    Wonderful typing 🚀

--- test/typescript.test.ts
+++ test/typescript.test.ts
@@ -29,3 +29,56 @@ describe("TypeScript", () => {
     type Special = S.Split<"a.b.c", ".">;
   });
 });
+
+type Address = {
+  postCode: string;
+  street: [string, string | undefined];
+};
+
+type UserInfo = {
+  address: Address;
+  previousAddress?: Address;
+  callMe: () => void;
+};
+
+const data: UserInfo = {
+  address: {
+    postCode: "SW1P 3PA",
+    street: ["20 Deans Yd", undefined],
+  },
+  callMe: () => {},
+};
+
+export type GetFieldType<Obj, Path> =
+  Path extends `${infer Left}.${infer Right}`
+    ? Left extends keyof Obj
+      ?
+          | GetFieldType<Exclude<Obj[Left], undefined>, Right>
+          | Extract<Obj[Left], undefined>
+      : undefined
+    : Path extends keyof Obj
+      ? Obj[Path]
+      : undefined;
+
+export function getValue<
+  TData,
+  TPath extends string,
+  TDefault = GetFieldType<TData, TPath>,
+>(
+  data: TData,
+  path: TPath,
+  defaultValue?: TDefault
+): GetFieldType<TData, TPath> | TDefault {
+  const value = path
+    .split(".")
+    .reduce<
+      GetFieldType<TData, TPath>
+    >((value, key) => (value as any)?.[key], data as any);
+
+  return value !== undefined ? value : (defaultValue as TDefault);
+}
+
+const value = getValue(data, "address.street.1231", () => {});
+//    ^?
+
+value();
