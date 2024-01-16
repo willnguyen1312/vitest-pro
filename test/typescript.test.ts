d962049e9586b112eb89028c573489df117aeffd
Nam Nguyen <nam.nguyen.node@gmail.com>
2024-01-16 17:00:28 -0400

    ts-toolbelt is awesome 🎉

--- test/typescript.test.ts
+++ test/typescript.test.ts
@@ -1,17 +1,31 @@
+import { O, S } from "ts-toolbelt";
+
 describe("TypeScript", () => {
   it("should work", () => {
     const source: number[] = [1, 2, 3];
     type DataItem = {
       id: number;
       name: string;
+      obj: {
+        wow: string;
+      };
     };
+
+    type DataItems = DataItem[];
 
     const data = source.map(
       (item) =>
         ({
           id: item,
           name: "name",
-        }) satisfies DataItem,
+          obj: {
+            wow: "wow",
+          },
+        }) satisfies DataItem
     );
+
+    type A = O.Path<DataItems, ["0", "obj"]>;
+
+    type Special = S.Split<"a.b.c", ".">;
   });
 });
