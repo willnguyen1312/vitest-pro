17c8e0c33aa86770941228ac793a9448e7d3324b
Nam Nguyen <nam.nguyen.node@gmail.com>
2024-01-19 10:26:55 -0400

    Spy is damn cool 🕵️

--- test/playground.test.ts
+++ test/playground.test.ts
@@ -1,15 +1,16 @@
-import { it, vi, expect } from "vitest";
-
-const obj = {
-  hi: () => "Hello",
-};
+import { vi } from "vitest";
+import * as stuff from "./sample.ts";
 
 it("should pass", () => {
-  const spied = vi.spyOn(obj, "hi");
+  const input = 1;
 
-  const result = obj.hi();
+  // spy
+  const doubleSpy = vi.spyOn(stuff, "double");
 
-  expect(result).toBe("Hello");
+  doubleSpy.mockReturnValue(20);
 
-  expect(spied).toHaveBeenCalledTimes(1);
+  const output = stuff.double(input);
+  expect(output).toEqual(20);
+
+  expect(doubleSpy).toHaveBeenCalledTimes(1);
 });
