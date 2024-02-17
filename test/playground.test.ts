356cf8fb92dbc3844bd72d7189faaef62827d88e
Nam Nguyen <nam.nguyen.node@gmail.com>
2024-02-16 22:10:23 -0400

    🚀

--- test/playground.test.ts
+++ test/playground.test.ts
@@ -1,16 +1,45 @@
 import { vi } from "vitest";
 import * as stuff from "./sample.ts";
 
-it("should pass", () => {
-  const input = 1;
+describe("sample", () => {
+  const mockFn = vi.fn(() => 20);
 
-  // spy
-  const doubleSpy = vi.spyOn(stuff, "double");
+  beforeEach(() => {
+    vi.resetAllMocks();
+  });
 
-  doubleSpy.mockReturnValue(20);
+  it("should pass once", () => {
+    mockFn.mockReturnValue(10);
+    const input = 1;
+    // spy
+    const doubleSpy = vi.spyOn(stuff, "double");
 
-  const output = stuff.double(input);
-  expect(output).toEqual(20);
+    doubleSpy.mockReturnValue(20);
 
-  expect(doubleSpy).toHaveBeenCalledTimes(1);
+    const output = stuff.double(input);
+    expect(output).toEqual(20);
+
+    expect(doubleSpy).toHaveBeenCalledTimes(1);
+
+    expect(mockFn()).toBe(10);
+    expect(mockFn).toHaveBeenCalledTimes(1);
+  });
+
+  it("should pass twice", () => {
+    mockFn();
+    const input = 1;
+    // spy
+    const doubleSpy = vi.spyOn(stuff, "double");
+
+    doubleSpy.mockReturnValue(20);
+
+    const output = stuff.double(input);
+    expect(output).toEqual(20);
+
+    expect(doubleSpy).toHaveBeenCalledTimes(1);
+
+    mockFn.mockReturnValue(10);
+    expect(mockFn).toHaveBeenCalledTimes(1);
+    expect(mockFn()).toBe(10);
+  });
 });
