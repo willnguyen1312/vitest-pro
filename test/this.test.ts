da397f7a00c9faa15fd1a8c875e504e5cf440622
Nam Nguyen <nam.nguyen@shopify.com>
2024-03-26 12:34:22 -0300

    Oh my JS 💞

---
+++ test/this.test.ts
@@ -0,0 +1,21 @@
+it("works for this", () => {
+  function Effect(this, fn: () => unknown) {
+    this._fn = fn;
+  }
+
+  Effect.prototype._callback = function () {
+    const cleanup = this._fn();
+  };
+
+  function effect(fun) {
+    const a = new Effect(fun);
+    a._callback();
+  }
+
+  let updater!: typeof Effect;
+  effect(function (this) {
+    updater = this;
+  });
+
+  expect(updater).toBeInstanceOf(Effect);
+});
