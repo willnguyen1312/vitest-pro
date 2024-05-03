29e35d2cd22a508851b84079def8f6dd8ca2bd95
Nam Nguyen <nam.nguyen@shopify.com>
2024-05-03 18:04:59 -0300

    Mock class with style xD

--- src/sum.ts
+++ src/sum.ts
@@ -1,3 +1,11 @@
 export function sum(a: number, b: number): number {
   return a + b;
 }
+
+export default class User {
+  constructor(private name: string) {}
+
+  getName() {
+    return this.name;
+  }
+}
--- test/mock.test.ts
+++ test/mock.test.ts
@@ -1,12 +1,19 @@
 import { vi } from "vitest";
-import { sum } from "../src/sum.ts";
+import User, { sum } from "../src/sum.ts";
 
-// Mock sum function with vitest
+vi.mock("../src/sum.ts");
 
 describe("mock", () => {
   it("should work", () => {
     const mockSum = vi.fn(sum);
     mockSum.mockReturnValue(3);
     expect(mockSum(1, 2)).toBe(3);
+  });
+
+  it("should work again", () => {
+    vitest.spyOn(User.prototype, "getName").mockReturnValue("Mocked");
+
+    const user = new User("Nam");
+    expect(user.getName()).toBe("Mocked");
   });
 });
