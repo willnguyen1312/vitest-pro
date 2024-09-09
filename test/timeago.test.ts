97b0ff5d0d44f9dfd72eb79c53f52fd99f13be4a
Nam Nguyen <nam.nguyen@shopify.com>
2024-09-09 17:44:53 -0600

    Let's go champ 🚀

--- test/timeago.test.ts
+++ test/timeago.test.ts
@@ -20,24 +20,65 @@ import { formatTimeAgo } from "./timeago";
 // // e.g.
 // format(Date.now() - 11 * 1000 * 60 * 60); // returns '11 hours ago'
 
-it("should format timestamp", async () => {
-  //   expect(format("2024-09-01T19:02:19Z")).toBe("2 days ago");
-  //   expect(format("2024-09-02T19:02:19Z")).toBe("1 day ago");
-  // expect(formatTimeAgo("2024-09-02T19:02:19Z")).toBe("1 day ago");
-  // expect(formatTimeAgo("2024-09-01T19:02:19Z")).toBe("2 day ago");
-  // expect(formatTimeAgo("2024-03-01T19:02:19Z")).toBe("186 day ago");
-  // const date = new Date();
-  // vi.useFakeTimers();
-  // vi.advanceTimersByTime(9000);
-  // expect(formatTimeAgo(date.toUTCString())).toBe("9 second ago");
+describe("timeago", () => {
+  beforeEach(() => {
+    vi.useRealTimers();
+  });
 
-  type Item = {
-    name: string;
-  };
+  it("should format a minute correctly", async () => {
+    const date = new Date();
+    vi.useFakeTimers();
+    vi.advanceTimersByTime(9000);
+    expect(formatTimeAgo(date.toUTCString())).toBe("1 minute ago");
+    vi.useRealTimers();
+  });
 
-  const list: Item[] = [{ name: "Nam" }];
+  it("should format minutes correctly", async () => {
+    const date = new Date();
+    vi.useFakeTimers();
+    vi.advanceTimersByTime(10 * 1000 * 60);
+    expect(formatTimeAgo(date.toUTCString())).toBe("11 minute ago");
+  });
 
-  const name = list[0]?.name;
+  it("should format an hour correctly", async () => {
+    const date = new Date();
+    vi.useFakeTimers();
+    vi.advanceTimersByTime(59 * 1000 * 60);
+    expect(formatTimeAgo(date.toUTCString())).toBe("1 hour ago");
+  });
 
-  console.log("value: ", name);
+  it("should format hours correctly", async () => {
+    const date = new Date();
+    vi.useFakeTimers();
+    vi.advanceTimersByTime(119 * 1000 * 60);
+    expect(formatTimeAgo(date.toUTCString())).toBe("2 hour ago");
+  });
+
+  it("should format a day correctly", async () => {
+    const date = new Date();
+    vi.useFakeTimers();
+    vi.advanceTimersByTime(23 * 60 * 1000 * 60);
+    expect(formatTimeAgo(date.toUTCString())).toBe("1 day ago");
+  });
+
+  it("should format days correctly", async () => {
+    const date = new Date();
+    vi.useFakeTimers();
+    vi.advanceTimersByTime(24 * 60 * 1000 * 60);
+    expect(formatTimeAgo(date.toUTCString())).toBe("2 day ago");
+  });
+
+  it("should format up to 6 days correctly", async () => {
+    const date = new Date();
+    vi.useFakeTimers();
+    vi.advanceTimersByTime(5 * 24 * 60 * 1000 * 60);
+    expect(formatTimeAgo(date.toUTCString())).toBe("6 day ago");
+  });
+
+  it("should format up to 6 days correctly", async () => {
+    const date = new Date();
+    vi.useFakeTimers();
+    vi.advanceTimersByTime(6 * 24 * 60 * 1000 * 60);
+    expect(formatTimeAgo(date.toUTCString())).toBe(null);
+  });
 });
--- test/timeago.ts
+++ test/timeago.ts
@@ -2,19 +2,16 @@ export const formatTimeAgo = (input: string): string => {
   const toDate = new Date(input);
   const current = new Date();
 
-  const diffInSeconds = (current.getTime() - toDate.getTime()) / 1000;
+  const diffInSeconds = (current.getTime() - toDate.getTime()) / (1000 * 60);
 
   // format it with locale
   return formatDiff(diffInSeconds);
 };
 
 const SEC_ARRAY = [
-  60, // number of seconds in 1 min
   60, // number of mins in 1 hour
   24, // number of hours in 1 day
-  // 7, // number of days in 1 week
-  // 365 / 7 / 12, // number of weeks in 1 month
-  // 12, // number of months in 1 year
+  7, // number of days in 1 week
 ];
 
 export function formatDiff(diff: number): string {
@@ -25,18 +22,21 @@ export function formatDiff(diff: number): string {
     idx += 1;
   }
 
-  diff = Math.floor(diff);
+  diff = Math.ceil(diff);
+
+  if (idx === 0 && diff === SEC_ARRAY[idx]) {
+    idx++;
+    diff = 1;
+  } else if (idx === 1 && diff === SEC_ARRAY[idx]) {
+    idx++;
+    diff = 1;
+  } else if (idx === 2 && diff === SEC_ARRAY[idx]) {
+    return null;
+  }
+
   return diff + " " + localeFunc(idx);
 }
 
 function localeFunc(index: number): string {
-  return [
-    "second ago",
-    "minute ago",
-    "hour ago",
-    "day ago",
-    "week ago",
-    "month ago",
-    "year ago",
-  ][index];
+  return ["minute ago", "hour ago", "day ago"][index];
 }
