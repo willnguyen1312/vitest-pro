d6cd76700377f179e98e51c9a18053789b3885fe
Nam Nguyen <nam.nguyen@shopify.com>
2024-09-10 20:16:57 -0600

    🤯

---
+++ test/timezone.ts
@@ -0,0 +1,17 @@
+function changeTimezone(date: Date, timeZone: string) {
+  // suppose the date is 12:00 UTC
+  const shiftedDate = new Date(
+    date.toLocaleString("en-US", {
+      timeZone,
+    })
+  );
+
+  // so 12:00 in Toronto is 17:00 UTC
+  return new Date(shiftedDate.getTime());
+}
+
+// E.g.
+var here = new Date();
+var there = changeTimezone(here, "America/Toronto");
+
+console.log(`Here: ${here.toString()}\nToronto: ${there.toString()}`);
