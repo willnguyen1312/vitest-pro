52a59bab716c6beff67f919dc6c2db74f69639ef
Nam Nguyen <nam.nguyen@shopify.com>
2024-09-10 18:30:43 -0600

    Simplicity is the best ✨

--- test/finalTimeAgo.test.ts
+++ test/finalTimeAgo.test.ts
@@ -1,36 +1,18 @@
 import { faker } from "@faker-js/faker";
 
-const rawDateStr = "2024-09-05T22:32:49.223Z";
+const formatTimeAgo = (past: string, from: string) => {
+  const toDate = new Date(past);
+  const current = new Date(from);
 
-const formatTimeAgo = (input: string) => {
-  const toDate = new Date(input);
-  const current = new Date(rawDateStr);
-
-  toDate.setHours(0, 0, 0, 0);
-  current.setHours(0, 0, 0, 0);
-
-  const differenceInMilliseconds = current.getTime() - toDate.getTime();
-
-  const millisecondsPerDay = 24 * 60 * 60 * 1000;
-  const differenceInDays = Math.floor(
-    differenceInMilliseconds / millisecondsPerDay
-  );
-
-  //   Over a week
-  if (differenceInDays >= 7) {
-    return toDate.toDateString();
-  }
-
-  if (differenceInDays > 0 && differenceInDays < 7) {
-    return differenceInDays === 1
-      ? "1 day ago"
-      : `${differenceInDays} days ago`;
-  }
+  const isSameDay =
+    toDate.getFullYear() === current.getFullYear() &&
+    toDate.getMonth() === current.getMonth() &&
+    toDate.getDate() === current.getDate();
 
   //   Same day
-  if (differenceInDays === 0) {
-    const toDate = new Date(input);
-    const current = new Date(rawDateStr);
+  if (isSameDay) {
+    const toDate = new Date(past);
+    const current = new Date(from);
 
     const differenceInMinutes = Math.floor(
       (current.getTime() - toDate.getTime()) / (1000 * 60)
@@ -55,13 +37,49 @@ const formatTimeAgo = (input: string) => {
 
     return `${differenceHoursStr} ${differenceHours} ago`;
   }
+
+  const isSameMonthOfTheTime =
+    toDate.getMonth() === current.getMonth() &&
+    toDate.getFullYear() === current.getFullYear();
+
+  const differenceInDays = current.getDate() - toDate.getDate();
+
+  //   Under a week same month
+  if (isSameMonthOfTheTime && differenceInDays < 7) {
+    return differenceInDays === 1
+      ? "1 day ago"
+      : `${differenceInDays} days ago`;
+  }
+
+  //   Under a week different month
+  if (!isSameMonthOfTheTime) {
+    // Calculate days to the end of the prev month
+    const daysInPrevMonth = new Date(
+      toDate.getFullYear(),
+      toDate.getMonth(),
+      0
+    ).getDate();
+    const daysToPrevMonthEnd = daysInPrevMonth - toDate.getDate();
+
+    // Calculate days from the start of the next month
+    const daysFromNextMonthStart = current.getDate();
+    const totalDays = daysToPrevMonthEnd + daysFromNextMonthStart;
+
+    if (totalDays < 7) {
+      return totalDays === 1 ? "1 day ago" : `${totalDays} days ago`;
+    }
+  }
+
+  return toDate.toDateString();
 };
 
 describe("formatTimeAgo", () => {
+  const rawDateStr = "2024-09-05T22:32:49.223Z";
+
   it("returns a full date when it is more than 6 days", () => {
     const date = new Date(rawDateStr);
     date.setDate(date.getDate() - 7);
-    const output = formatTimeAgo(date.toISOString());
+    const output = formatTimeAgo(date.toISOString(), rawDateStr);
     expect(output).toBe("Thu Aug 29 2024");
   });
 
@@ -71,7 +89,7 @@ describe("formatTimeAgo", () => {
       const date = new Date(rawDateStr);
       const randomDays = faker.number.int({ min: 1, max: 6 });
       date.setDate(date.getDate() - randomDays);
-      const output = formatTimeAgo(date.toISOString());
+      const output = formatTimeAgo(date.toISOString(), rawDateStr);
       const expected =
         randomDays === 1 ? "1 day ago" : `${randomDays} days ago`;
       expect(output).toBe(expected);
@@ -84,7 +102,7 @@ describe("formatTimeAgo", () => {
       const date = new Date(rawDateStr);
       const randomSeconds = faker.number.int({ min: 0, max: 119 });
       date.setUTCSeconds(date.getUTCSeconds() - randomSeconds);
-      const output = formatTimeAgo(date.toISOString());
+      const output = formatTimeAgo(date.toISOString(), rawDateStr);
       expect(output).toBe("1 minute ago");
     }
   });
@@ -95,7 +113,7 @@ describe("formatTimeAgo", () => {
       const date = new Date(rawDateStr);
       const randomSeconds = faker.number.int({ min: 120, max: 3599 });
       date.setUTCSeconds(date.getUTCSeconds() - randomSeconds);
-      const output = formatTimeAgo(date.toISOString());
+      const output = formatTimeAgo(date.toISOString(), rawDateStr);
       const expected = `${Math.floor(randomSeconds / 60)} minutes ago`;
       expect(output).toBe(expected);
     }
@@ -107,7 +125,7 @@ describe("formatTimeAgo", () => {
       const date = new Date(rawDateStr);
       const randomSeconds = faker.number.int({ min: 3600, max: 7199 });
       date.setUTCSeconds(date.getUTCSeconds() - randomSeconds);
-      const output = formatTimeAgo(date.toISOString());
+      const output = formatTimeAgo(date.toISOString(), rawDateStr);
       expect(output).toBe("1 hour ago");
     }
   });
@@ -118,7 +136,7 @@ describe("formatTimeAgo", () => {
       const date = new Date(rawDateStr);
       const randomHours = faker.number.int({ min: 2, max: 10 });
       date.setUTCHours(date.getUTCHours() - randomHours);
-      const output = formatTimeAgo(date.toISOString());
+      const output = formatTimeAgo(date.toISOString(), rawDateStr);
       const expected = `${Math.floor(randomHours)} hours ago`;
       expect(output).toBe(expected);
     }
