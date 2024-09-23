95678720186b1efb8c2c578a730a3d2c290fc23d
Nam Nguyen <nam.nguyen@shopify.com>
2024-09-23 16:09:14 -0600

    Let's sort xD

--- test/sort.test.ts
+++ test/sort.test.ts
@@ -19,3 +19,36 @@ it("should work on custom sort", () => {
   const expected = [5, 2, 3, 4, 1];
   expect(result).toEqual(expected);
 });
+
+it("should work on custom sort", () => {
+  type Item = {
+    name: string;
+    count: number;
+  };
+
+  const original: Item[] = [
+    { name: "a", count: 1 },
+    { name: "b", count: 3 },
+    { name: "c", count: 5 },
+    { name: "d", count: 2 },
+    { name: "e", count: 4 },
+  ];
+
+  const sorted = original.toSorted((a, b) => a.count - b.count);
+
+  expect(sorted).toEqual([
+    { name: "a", count: 1 },
+    { name: "d", count: 2 },
+    { name: "b", count: 3 },
+    { name: "e", count: 4 },
+    { name: "c", count: 5 },
+  ]);
+
+  expect(original).toEqual([
+    { name: "a", count: 1 },
+    { name: "b", count: 3 },
+    { name: "c", count: 5 },
+    { name: "d", count: 2 },
+    { name: "e", count: 4 },
+  ]);
+});
