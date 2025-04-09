5d02fe86d36dbdccafe9bca64ed21660d9b805f8
Nam Nguyen <nam.nguyen@shopify.com>
2025-04-08 21:31:04 -0600

    😎

--- test/async.test.ts
+++ test/async.test.ts
@@ -1,28 +1,39 @@
 import { describe, vi, it } from "vitest";
 
+let total = 0;
+let count = 0;
+
+async function act(callback: Function) {
+  // Check if callback is returning a promise
+  const result = callback();
+
+  if (result instanceof Promise) {
+    console.log("promise");
+    await result;
+  }
+
+  console.log("applying changes");
+  await new Promise((resolve) => setTimeout(resolve, 100));
+  total = count;
+  return void 0;
+}
+
 describe("async", () => {
-  const asyncFn = async () => {
-    // Promise.resolve().then(() => {
-    //   console.log("resolve late");
-    // });
-
-    new Promise((resolve) => {
-      console.log("new promise");
-      setTimeout(() => {
-        console.log("resolve");
-        resolve(void 0);
-      }, 1000);
+  const asyncFn = () => {
+    Promise.resolve().then(() => {
+      console.log("resolve late 1");
+      count++;
     });
-
-    console.log("end");
-    return "ok";
+    Promise.resolve().then(() => {
+      console.log("resolve late 2");
+      count++;
+    });
+    return Promise.resolve("ok");
   };
 
   it("should be ok", async () => {
-    vi.useFakeTimers();
-    await asyncFn();
-    console.log("done");
-    await vi.runAllTimersAsync();
-    // expect(result).toBe("ok");
+    await act(asyncFn);
+
+    expect(total).toBe(2);
   });
 });
