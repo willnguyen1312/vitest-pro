6ed1e6f2856201f564d50441112ea4dd9f49183e
Nam Nguyen <nam.nguyen@shopify.com>
2025-04-23 13:44:44 -0600

    🤩

--- test/performance.test.ts
+++ test/performance.test.ts
@@ -4,7 +4,7 @@ test("performance", async () => {
   performance.mark("start");
 
   //   Block 2s
-  await new Promise((resolve) => setTimeout(resolve, 2000));
+  await new Promise((resolve) => setTimeout(resolve, 500));
 
   performance.mark("end");
   console.log(performance.measure("test", "start", "end"));
