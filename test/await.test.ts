53dfdb5036edec84d4746c805ff39d2611cbf06a
Nam Nguyen <nam.nguyen@shopify.com>
2025-08-15 11:59:39 -0600

    🚀

--- test/await.test.ts
+++ test/await.test.ts
@@ -44,3 +44,34 @@ test("graphql client multiple calls", async () => {
   expect(result.helloResult).toBe("Hello, world!");
   expect(result.hiResult).toBe("Hi, world!");
 });
+
+const sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
+
+test("loops with await", async () => {
+  // Spy on console.log
+  const consoleLogSpy = vi.spyOn(console, "log").mockImplementation(() => {});
+
+  vi.useFakeTimers();
+  const waitTimes = [4000, 3000, 2000];
+
+  const promises = Promise.all(
+    waitTimes.map(async (waitTime) => {
+      await sleep(waitTime);
+      console.log(`waited for ${waitTime}ms`);
+    })
+  );
+
+  vi.runAllTimers();
+
+  await promises;
+
+  expect(consoleLogSpy).toHaveBeenCalledTimes(3);
+  expect(consoleLogSpy).toHaveBeenNthCalledWith(1, "waited for 2000ms");
+  expect(consoleLogSpy).toHaveBeenNthCalledWith(2, "waited for 3000ms");
+  expect(consoleLogSpy).toHaveBeenNthCalledWith(3, "waited for 4000ms");
+
+  vi.useRealTimers();
+  console.log("before restored");
+  consoleLogSpy.mockRestore();
+  console.log("after restored");
+});
