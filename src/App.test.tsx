5d6095ff802501d0ee1547f3243e2ded6deaaa00
Nam Nguyen <nam.nguyen@shopify.com>
2025-08-21 13:24:31 -0600

    🚀

---
+++ src/App.test.tsx
@@ -0,0 +1,11 @@
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import App from "./App";
+
+it("should work great", async () => {
+  render(<App />);
+  const user = userEvent.setup();
+  const button = screen.getByRole("button");
+  await user.click(button);
+  expect(screen.getByText("Hello World")).toBeInTheDocument();
+});
