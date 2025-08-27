325c6c1d55a3aca616c6cae2c7aa6320288a7f51
Nam Nguyen <nam.nguyen.node@gmail.com>
2025-08-26 23:14:04 -0600

    🤘

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
+import { useReducer } from "react";
+
 export default function App() {
+  const [state, dispatch] = useReducer((a: number) => a + 1, 0);
   return (
     <div>
       <h1>Hello World</h1>
-      <button>Click me</button>
+      <button onClick={() => dispatch()}>Click me</button>
     </div>
   );
 }
