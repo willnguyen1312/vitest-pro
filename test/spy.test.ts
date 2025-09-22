0defd6540aed40f5842496dfbcbe08c59d329e12
Nam Nguyen <nam.nguyen.node@gmail.com>
2025-09-22 16:56:09 -0600

    ✨

--- test/spy.test.ts
+++ test/spy.test.ts
@@ -6,13 +6,21 @@ const obj = {
   },
 };
 
+const callMeBaby = () => "Call me baby";
+
 describe("spy", () => {
   it("should work", () => {
     const spy = vi.spyOn(obj, "getName");
     const result = obj.getName();
     expect(obj.getName()).toBe("Nam");
-    expect(spy).toHaveBeenCalled();
+    expect(spy).toHaveBeenCalledTimes(2);
 
     expect(result).toBe("Nam");
+  });
+
+  it("should work with callMeBaby", () => {
+    const spy = vi.fn(callMeBaby);
+    expect(spy()).toBe("Call me baby");
+    expect(spy).toHaveBeenCalledTimes(1);
   });
 });
