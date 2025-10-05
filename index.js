f4f6bf0367f6b6d335a622bd5e7a7db7029d7a2c
Nam Nguyen <nam.nguyen.node@gmail.com>
2025-10-05 14:03:52 -0600

    ✨

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
-process.nextTick(() => {
-  console.log("1");
-});
+const first = () => {
+  console.log("first");
+};
 
-console.log("2");
+const second = () => {
+  console.log("second");
+};
+
+const arrs = [first, second];
+
+const filterArrs = arrs.filter((item) => item !== second);
+
+filterArrs.forEach((fn) => fn());
+
+console.log("arrs", arrs.length);
+console.log("filterArrs", filterArrs.length);
