42e7221dec709ff53a2f3bcb7024f9f3ae573a2e
Nam Nguyen <nam.nguyen@shopify.com>
2025-11-26 13:34:58 -0700

    🎊

---
+++ test/fetch.test.ts
@@ -0,0 +1,21 @@
+import { test } from "vitest";
+import axios, { AxiosError } from "axios";
+
+test("fetch", async () => {
+  const response = await fetch(
+    "https://jsonplaceholder.typicode.com/todos/1000"
+  );
+  expect(response.status).toBe(404);
+});
+
+test("axios", async () => {
+  ``;
+  try {
+    await axios.get("https://jsonplaceholder.typicode.com/todos/1000");
+  } catch (error) {
+    const isAxiosError = axios.isAxiosError(error);
+    if (isAxiosError) {
+      expect(error.status).toBe(404);
+    }
+  }
+});
