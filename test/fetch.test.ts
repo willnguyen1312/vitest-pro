5835dd16687fb79166ff1bfe53eee977f746d553
Nam Nguyen <nam.nguyen@shopify.com>
2025-11-26 13:36:34 -0700

    🏃

--- test/fetch.test.ts
+++ test/fetch.test.ts
@@ -1,16 +1,16 @@
 import { test } from "vitest";
 import axios from "axios";
 
+const invalid404Url = "https://jsonplaceholder.typicode.com/todos/1000";
+
 test("fetch", async () => {
-  const response = await fetch(
-    "https://jsonplaceholder.typicode.com/todos/1000"
-  );
+  const response = await fetch(invalid404Url);
   expect(response.status).toBe(404);
 });
 
 test("axios", async () => {
   try {
-    await axios.get("https://jsonplaceholder.typicode.com/todos/1000");
+    await axios.get(invalid404Url);
   } catch (error) {
     const isAxiosError = axios.isAxiosError(error);
     if (isAxiosError) {
