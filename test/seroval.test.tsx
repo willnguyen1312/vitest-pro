c22bd25950fda5aafa1c1027786b2458272700f9
Nam Nguyen <nam.nguyen@shopify.com>
2026-02-19 18:44:45 +0700

    🤩

---
+++ test/seroval.test.tsx
@@ -0,0 +1,23 @@
+import { describe, it } from "vitest";
+import { createReference, serialize, js, fromJSON, toJSON, deserialize } from "seroval";
+
+describe("seroval", () => {
+    it("should serialize the object", async () => {
+        // console.log(serialize({ greet: () => "Hello" }));
+        // Register the function with a unique ID
+        const greet = createReference("greet", () => "Hello");
+
+        // Now you can serialize it
+        const result = serialize(greet);
+        console.log(result);
+        console.log((deserialize(result) as () => string)());
+        // console.log(result);
+
+        // // we can now serialize this
+        // const serialized = toJSON(greet); // or any of the serializer
+        // // true
+        // console.log(greet === fromJSON(serialized));
+        // console.log(toJSON(greet));
+        // console.log((fromJSON(serialized) as () => string)());
+    });
+});
