877770e2ca12df6d32fe8106f6dc81ca183979ce
Nam Nguyen <nam.nguyen@shopify.com>
2026-05-11 22:22:51 -0600

    🕵️

--- test/closure.test.ts
+++ test/closure.test.ts
@@ -1,3 +1,5 @@
+import { vi } from "vitest";
+
 it("should work like a closure", () => {
   const useValue = (fn: () => number) => {
     const rerun = fn;
@@ -13,4 +15,27 @@ it("should work like a closure", () => {
   rerun();
 
   expect(value).toBe(1);
+});
+
+it("should work like a closure two", () => {
+  const spiedConsoleLog = vi.spyOn(console, "log");
+  const items = [1, 2, 3];
+
+  const transformedItems = items.map((item) => {
+    return {
+      value: item,
+      trigger: () => {
+        console.log("triggered ", item);
+      },
+      triggerAll: () => {
+        transformedItems.forEach((item) => item.trigger());
+      },
+    };
+  });
+
+  transformedItems[1].triggerAll();
+  expect(spiedConsoleLog).toHaveBeenCalledTimes(3);
+  expect(spiedConsoleLog).toHaveBeenNthCalledWith(1, "triggered ", 1);
+  expect(spiedConsoleLog).toHaveBeenNthCalledWith(2, "triggered ", 2);
+  expect(spiedConsoleLog).toHaveBeenNthCalledWith(3, "triggered ", 3);
 });
