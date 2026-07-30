05f13f34cbfe1d0b1f1b866dc5501e81b351921f
Nam Nguyen <nam.nguyen.node@gmail.com>
2026-07-30 10:48:53 -0600

    🛁

--- test/zod.test.ts
+++ test/zod.test.ts
@@ -4,7 +4,7 @@ import { z } from "zod";
 it("zod works well", async () => {
   const UserSchema = z.object({
     username: z.string().min(3).max(20),
-    email: z.string().email(),
+    email: z.email(),
     age: z.number().min(18).optional(),
   });
 
